a267f54f82792e063e4e67a20cef0712a86f18c1
Max <masamune.code@gmail.com>
2021-04-02 12:31:38 +0200

    💥MOD: Connexion a la bdd réussie !

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host       : 'localhost'
+  ,user      : 'root'
+  ,password  : ''
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log('Connecté à la base de données MySQL!');
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
