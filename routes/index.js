7a0496de586e43ba83ea6e7eaf8c8331b39347c5
Max <masamune.code@gmail.com>
2021-04-02 12:44:57 +0200

    🔥MOD: Affichage basique dans le html

--- routes/index.js
+++ routes/index.js
@@ -18,13 +18,21 @@ db.connect(function(err) {
   // On test une première requête bateau
   db.query('SELECT * FROM `products`', function (err, result) {
     if (err) throw err;
-    console.log(result);
+    
+    // Asynchrone: les trucs de la bdd on bien été récupérés
+    console.log('ICI', result);
+
+    // Je m'en sers pour faire l'affichage
+    /* GET home page. */
+    router.get('/', function(req, res, next) {
+      res.render('index', { title: 'Test affichage depuis la bdd', lesDonneesQuiViennentDeLaBDD: result });
+    });
   });
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//// /!\ ATTENTION /!\
+//  Nous sommes en asynchrone, les données ne seront pas dispo ici
+// mais uniquement dans le callback, bizoux
+// console.log('LA', result);
 
 module.exports = router;
